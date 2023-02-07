859d4daed09c15559727774b293f12831238986e
SpaceTubeDevs <germain.a13@gmail.com>
2023-02-07 22:53:34 +0100

    first commit

---
+++ website/index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "172.23.0.2";
+$username = "spacetube";
+$password = "antoine";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=toto", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
